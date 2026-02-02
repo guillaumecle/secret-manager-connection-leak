8ef6b7272f6152b74eca2c800e04be5ae6ef2530
cguillaume <clementg@zillowgroup.com>
2026-02-02 11:44:23 -0800

    support multiple calls

--- src/main/java/SecretManagerConnectionLeak.java
+++ src/main/java/SecretManagerConnectionLeak.java
@@ -4,12 +4,16 @@ import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueReques
 public class SecretManagerConnectionLeak{
 
 	public static void main(String[] args){
+		int count = args.length > 1 ? Integer.parseInt(args[1]) : 1;
+
 		try(SecretsManagerClient client = SecretsManagerClient.builder().build()){
 			GetSecretValueRequest request = GetSecretValueRequest.builder().secretId(args[0]).build();
-			try{
-				client.getSecretValue(request);
-			}catch(RuntimeException e){
-				System.out.println(e.toString());
+			for(int i = 0; i < count; ++i){
+				try{
+					client.getSecretValue(request);
+				}catch(RuntimeException e){
+					System.out.println(e.getClass().getSimpleName());
+				}
 			}
 		}
 	}
